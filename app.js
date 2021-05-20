7a3e64f4e41457d7708750f217e5f9e6a763c2a4
JoshuaJHaines <joshjhaines3@gmail.com>
2021-05-20 11:34:44 -0400

    spinning up server, pulling in express and logger

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+
+app.listen(3000, function () {
+	console.log("Server started in port 3000");
+});
